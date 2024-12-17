7b44f8cafc6baefa5a1bf0d1e7547270d5843c0b
hiepduc24089 <146322141+hiepduc24089@users.noreply.github.com>
2024-12-17 15:46:46 +0700

    home banner

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,7 @@ import "./bootstrap";
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import 'swiper/swiper-bundle.css';
 
 const application = createApp(App).use(router);
 application.mount("#app");
