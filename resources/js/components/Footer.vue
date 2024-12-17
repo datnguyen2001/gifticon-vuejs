99e8e12caaeae7c39caf7bcaeca927065b214a0d
datnguyen2001 <nguyendatdz01@gmail.com>
2024-12-17 16:52:51 +0700

    header

--- resources/js/components/Footer.vue
+++ resources/js/components/Footer.vue
@@ -1,23 +1,19 @@
-<script>
+<script setup>
 import bgFooter from "@/assets/bg-footer.png";
 import iconPhone from "@/assets/icon-phone.png";
+import iconMap from "@/assets/icon-map.png";
+import iconMail from "@/assets/icon-mail.png";
+import bgFooterBottom from "@/assets/bg-footer-bottom.png";
 
-export default {
-    data() {
-        return {
-            bgFooter,
-            iconPhone,
-        };
-    },
-};
+let items = [2, 2];
 </script>
 
 <template>
     <footer
-        class="w-full bg-no-repeat pt-8 mt-10"
+        class="w-full bg-no-repeat pt-8 mt-10 bg-contain"
         :style="{ backgroundImage: `url(${bgFooter})` }"
     >
-        <div class="w-[1400px] m-auto">
+        <div class="w-[1300px] m-auto">
             <div class="text-logo font-semibold text-[#F1641E] text-[40px]">
                 Gifticon
             </div>
@@ -41,27 +37,57 @@ export default {
                     </div>
                 </div>
                 <div class="w-1/3 flex gap-2">
-                    <img :src="iconPhone" alt="" class="w-[32px] h-[32px]" />
+                    <img :src="iconMail" alt="" class="w-[32px] h-[32px]" />
                     <div class="flex flex-col gap-1">
-                        <span class="text-[#9A9EA6] text-[14px]"
-                            >Điện thoại</span
-                        >
+                        <span class="text-[#9A9EA6] text-[14px]">Email</span>
                         <span class="text-[#F1641E] text-[16px] font-medium"
-                            >0123456789</span
+                            >ILvietnam@gmail.com</span
                         >
                     </div>
                 </div>
                 <div class="w-1/3 flex gap-2">
-                    <img :src="iconPhone" alt="" class="w-[32px] h-[32px]" />
+                    <img :src="iconMap" alt="" class="w-[32px] h-[32px]" />
                     <div class="flex flex-col gap-1">
-                        <span class="text-[#9A9EA6] text-[14px]"
-                            >Điện thoại</span
-                        >
+                        <span class="text-[#9A9EA6] text-[14px]">Địa chỉ</span>
                         <span class="text-[#F1641E] text-[16px] font-medium"
-                            >0123456789</span
+                            >V7-B7 The Terra An Hưng La Khe, Hà Đông, Hà
+                            Nội</span
                         >
                     </div>
                 </div>
+            </div>
+
+            <div class="flex justify-between items-start mt-16">
+                <div class="w-1/3 flex flex-col gap-2">
+                    <p class="pb-3 text-[20px] font-semibold">
+                        Hỗ trợ khách hàng
+                    </p>
+                    <div v-for="item in items" :key="item.id">
+                        Các câu hỏi thường gặp
+                    </div>
+                </div>
+                <div class="w-1/3 flex flex-col gap-2">
+                    <p class="pb-3 text-[20px] font-semibold">Về chúng tôi</p>
+                    <div v-for="item in items" :key="item.id">
+                        Các câu hỏi thường gặp
+                    </div>
+                </div>
+                <div class="w-1/3 flex flex-col gap-2">
+                    <p class="pb-3 text-[20px] font-semibold">
+                        Kết nối với chúng tôi
+                    </p>
+                    <div v-for="item in items" :key="item.id">
+                        Các câu hỏi thường gặp
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="relative mt-4">
+            <img :src="bgFooterBottom" alt="" class="w-full" />
+            <div
+                class="absolute top-[60%] left-[50%] translate-y-[-50%] translate-x-[-50%] w-[1300px] m-auto text-[#FFFFFF]"
+            >
+                © 2023. All Rights Reserved..
             </div>
         </div>
     </footer>
